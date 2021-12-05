77b5667b0f3c2c6baf7b074e15ee8ec07f00beec
Sourabh Rajendra Chavan <sourabhchavan00@gmail.com>
2021-12-06 03:10:35 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+};
+
+export default App;
